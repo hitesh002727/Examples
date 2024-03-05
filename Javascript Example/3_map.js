b3b87326a88f60f392e7d92d7d852a4b30135a06
hitesh002727 <ckp1067@gmail.com>
2024-03-05 17:21:34 +0530

    javascript

---
+++ Javascript Example/3_map.js
@@ -0,0 +1,8 @@
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+]);
+
+let result = fruits.get("oranges");
+console.log(result)
---
+++ Javascript Example/4_if-else.js
@@ -0,0 +1,4 @@
+const hours = new Date().getHours();
+if (hours > 18)
+    console.log("Good Evening")
+else console.log("good day")
